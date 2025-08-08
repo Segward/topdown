dd17934482a85d662a61078195276164ce45e0b6
Segward <gustask@stud.ntnu.no>
2025-08-08 19:46:12 +0200

    Changed to sdl3

---
+++ README.md
@@ -0,0 +1,14 @@
+# Topdown
+
+## How to build
+```sh
+cd build
+cmake ..
+make
+````
+
+## How to run
+```sh
+cd build
+./topdown
+```
---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SDL3/SDL.h>
+
+int main(int argc, char *argv[])
+{
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window *window = SDL_CreateWindow("topdown", 800, 600, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+  SDL_Event event;
+  int running = 1;
+
+  while (running)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_EVENT_QUIT)
+      {
+        running = 0;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+
+    // Draw your game objects here
+
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
