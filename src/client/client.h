04bfa1a19e9df34f0331b63c2469a58699312478
Segward <gustask@stud.ntnu.no>
2025-08-15 04:22:56 +0200

    Client header file with send and receive

--- src/client/client.h
+++ src/client/client.h
@@ -2,6 +2,7 @@
 
 #include "pch.h"
 #include "player.h"
+#include "packet.h"
 
 #define PORT 8080
 
@@ -9,3 +10,18 @@ typedef struct {
   const int serverFd;
   player_t player;
 } client_t;
+
+static inline int send_packet(client_t *client, 
+  const struct sockaddr_in *addr, packet_t *packet) {
+  ssize_t bytes = sendto(client->serverFd, packet, sizeof(*packet), 0, 
+    (const struct sockaddr *)addr, sizeof(*addr));
+  return bytes;
+}
+
+static inline int receive_packet(client_t *client,
+  const struct sockaddr_in *addr, packet_t *packet) {
+  socklen_t addrLen = sizeof(*addr);
+  ssize_t bytes = recvfrom(client->serverFd, packet, sizeof(*packet), 0, 
+    (struct sockaddr *)addr, &addrLen);
+  return bytes;
+}
